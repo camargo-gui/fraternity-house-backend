08b5fbd9747ce7e282ed36859635e7b85b060cf1
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-03-21 23:05:25 -0300

    feat: adding cors origin

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
+import cors from "cors";
 import "dotenv/config";
 import loginRoutes from "employee/routes/employee-login-routes";
 import employeeRoutes from "employee/routes/employee-routes";
 import express from "express";
 import { DateTime } from "luxon";
 import medicineRoutes from "medicine/routes/medicine-routes";
+import pharmacologicalNameRoutes from "pharmacological-name/routes/pharmacological-name-routes";
 import residentRoutes from "resident/routes/resident-routes";
 import roleRoutes from "role/routes/role-routes";
-import pharmacologicalNameRoutes from "pharmacological-name/routes/pharmacological-name-routes";
 
 DateTime.local().setZone("America/Sao_Paulo");
 
 const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(express.json());
 
