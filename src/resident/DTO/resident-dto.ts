dde3a0311887f11cc342d5f4963d85d8595e3696
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-03-26 16:22:43 -0300

    feat: aws s3 bucket service to resident image

--- src/resident/DTO/resident-dto.ts
+++ src/resident/DTO/resident-dto.ts
@@ -4,6 +4,7 @@ export interface ResidentDTO {
   name: string;
   contact_phone: string;
   birthday: Date;
+  url_image: string | null;
   id?: number;
   created_a?: Date | null;
   updated_at?: Date | null;
