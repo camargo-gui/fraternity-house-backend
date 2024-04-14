6f14b67cd2ee520cc5d9b41cfa363da25f7210f8
camargo-rubcube <guilherme.camargo@rubcube.com>
2024-04-14 10:50:20 -0300

    chore: change employee authMiddleware

--- src/employee/routes/employee-routes.ts
+++ src/employee/routes/employee-routes.ts
@@ -1,15 +1,17 @@
 import { EmployeeController } from "employee/controller/employee-controller";
+import { EmployeeAuthMiddleware } from "employee/middleware/employee-auth-middleware";
 import { ValidateEmployeeDataMiddleware } from "employee/middleware/validate-employee-data-middleware";
 import { Router } from "express";
 
 
 const routes = Router();
 const controller = new EmployeeController();
-const middleware = new ValidateEmployeeDataMiddleware();
+const authMiddleware = new EmployeeAuthMiddleware();
+const valiidateEmployeeMiddleware = new ValidateEmployeeDataMiddleware();
 
-routes.post("/", middleware.execute, controller.create);
-routes.get("/", middleware.execute, controller.getAll);
-routes.delete("/:document", middleware.execute, controller.delete);
-routes.put("/", middleware.execute, controller.update);
+routes.post("/", valiidateEmployeeMiddleware.execute, controller.create);
+routes.get("/", authMiddleware.execute, controller.getAll);
+routes.delete("/:document", authMiddleware.execute, controller.delete);
+routes.put("/", authMiddleware.execute, controller.update);
 
 export default routes;
