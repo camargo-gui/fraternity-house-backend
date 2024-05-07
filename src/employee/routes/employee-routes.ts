e870235c1f9b739e52b209fa7241269822b5d639
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-05-07 00:23:34 -0300

    feat: role authorization middleware

--- src/employee/routes/employee-routes.ts
+++ src/employee/routes/employee-routes.ts
@@ -1,17 +1,30 @@
 import { EmployeeController } from "employee/controller/employee-controller";
 import { EmployeeAuthMiddleware } from "employee/middleware/employee-auth-middleware";
+import { RoleAuthorizationMiddleware } from "employee/middleware/role-authorization-middleware";
 import { ValidateEmployeeDataMiddleware } from "employee/middleware/validate-employee-data-middleware";
 import { Router } from "express";
-
+import { RoleEnum } from "role/DTO/role-dto";
 
 const routes = Router();
 const controller = new EmployeeController();
 const authMiddleware = new EmployeeAuthMiddleware();
-const valiidateEmployeeMiddleware = new ValidateEmployeeDataMiddleware();
+const validateEmployeeMiddleware = new ValidateEmployeeDataMiddleware();
+const adminAuth = new RoleAuthorizationMiddleware(RoleEnum.Administrador);
 
-routes.post("/", authMiddleware.execute, valiidateEmployeeMiddleware.execute, controller.create);
+routes.post(
+  "/",
+  authMiddleware.execute,
+  validateEmployeeMiddleware.execute,
+  adminAuth.execute,
+  controller.create
+);
 routes.get("/", authMiddleware.execute, controller.getAll);
-routes.delete("/:document", authMiddleware.execute, controller.delete);
-routes.put("/", authMiddleware.execute, controller.update);
+routes.delete(
+  "/:document",
+  authMiddleware.execute,
+  adminAuth.execute,
+  controller.delete
+);
+routes.put("/", authMiddleware.execute, adminAuth.execute, controller.update);
 
 export default routes;
--- src/movimentation/routes/movimentation-routes.ts
+++ src/movimentation/routes/movimentation-routes.ts
@@ -1,12 +1,23 @@
 import { EmployeeAuthMiddleware } from "employee/middleware/employee-auth-middleware";
+import { RoleAuthorizationMiddleware } from "employee/middleware/role-authorization-middleware";
 import { Router } from "express";
 import { MovimentationController } from "movimentation/controller/movimentation-controller";
+import { RoleEnum } from "role/DTO/role-dto";
 
 const routes = Router();
 const middleware = new EmployeeAuthMiddleware();
 const control = new MovimentationController();
+const authAdminAndEmployee = new RoleAuthorizationMiddleware(
+  RoleEnum.Administrador,
+  RoleEnum.Funcionario
+);
 
-routes.post("/", middleware.execute, control.create);
+routes.post(
+  "/",
+  middleware.execute,
+  authAdminAndEmployee.execute,
+  control.create
+);
 routes.get("/", control.getMovimentations);
 
 export default routes;
