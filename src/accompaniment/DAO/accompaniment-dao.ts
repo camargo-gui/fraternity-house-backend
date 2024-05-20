4d08e33e767bf26724dcbfb24cd181e760d8e52c
camargo-rubcube <guilherme.camargo@rubcube.com>
2024-05-20 11:04:34 -0300

    fix: get all residents has accompaniments

--- src/accompaniment/DAO/accompaniment-dao.ts
+++ src/accompaniment/DAO/accompaniment-dao.ts
@@ -53,7 +53,7 @@ export class AccompaninmentDAO {
     const result = await client.query(
       `SELECT r."id", r."name" as "residentName" FROM "Resident" r
       INNER JOIN "Accompaniment" a ON r."id" = a."residentId"
-      WHERE a."type" = $1 r."status" = 'ACTIVE'
+      WHERE a."type" = $1 AND r."status" = 'ACTIVE'
       GROUP BY r."id"`,
       [type]
     );
